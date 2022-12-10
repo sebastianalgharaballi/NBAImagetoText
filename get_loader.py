72b08144e64f6abe286339461e37b7f2cd985990
sebastianalgharaballi <95605946+sebastianalgharaballi@users.noreply.github.com>
2022-12-09 18:41:15 -0800

    Update get_loader.py

--- get_loader.py
+++ get_loader.py
@@ -1,3 +1,5 @@
+# This is a file that functions as a vocabulary builder for the project. It tokenizes the captions from my captions text file, and vectorizes the respesctive
+# tokens into batches to prepare for the training process.
 import os
 import pandas as pd
 import spacy  # tokenize words
